aa74f145853378db6326f05d29d29f5506901e2e
VadimAnciferov <vadim.anc@gmail.com>
2020-11-02 12:32:03 +0600

    VADIM DANIL AND KAZAH

---
+++ desktop/src/my/game/desktop/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package my.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.game.FlappyDemo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.fandroid.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyDemo.WIDTH;
+		config.height = FlappyDemo.HEIGHT;
+		config.title = FlappyDemo.TITLE;
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
